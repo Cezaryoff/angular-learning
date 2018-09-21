9a7763337e150de89566206b4b0ae1d714e7effe
Cezary Charusta <cezary.charusta@pb.com>
2018-09-21 12:48:21 +0200

    All files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html', //this is default place for html component
+  // template: '<h1>Hello World!</h1>',  single line
+  template:
+      `<app-first-four-lessons></app-first-four-lessons>
+  <app-five-to></app-five-to>,
+  <app-ten-animations></app-ten-animations>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCourse';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { FirstFourLessonsComponent } from './first-four-lessons/first-four-lessons.component';
+import { FiveToComponent } from './five-events/five-to.component';
+import {DataService} from './data.service';
+import { TenAnimationsComponent } from './ten-animations/ten-animations.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpModule} from '@angular/http';
+
+// import { DataService} from './data.service'; it is not necessaary
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    FirstFourLessonsComponent,
+    FiveToComponent,
+    TenAnimationsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
